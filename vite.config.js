c67406a6ba2cca4dc13b7dd85a6a2e0df65fcdfe
irvin <meunier.irvin@orange.fr>
2026-01-21 22:08:51 +0100

    V2.1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Configuration pour GitHub Pages avec le nom de ton dépôt
   base: '/calendrier-vaccinal/',
 
   plugins: [react()],
@@ -14,7 +12,8 @@ export default defineConfig({
   },
 
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
