fff33335a0bd004956838b77ace5ae0ca3f1f435
irvin <meunier.irvin@orange.fr>
2026-01-21 21:43:03 +0100

    V2.1

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 1. Définissez la base pour le déploiement
-  // Si vous déployez sur https://nom-utilisateur.github.io/mon-projet/
-  // Remplacez 'mon-projet' par le nom exact de votre dépôt.
-  // Si vous déployez sur un domaine racine (ex: Vercel ou Netlify), utilisez '/'
-  base: '/nom-du-repo-github/',
+  // Configuration pour GitHub Pages avec le nom de ton dépôt
+  base: '/calendrier-vaccinal/',
 
   plugins: [react()],
 
   server: {
-    // Optionnel : pour ouvrir automatiquement le navigateur en local
     open: true,
     port: 3000,
   },
 
   build: {
-    // Optionnel : pour s'assurer que les assets sont bien générés
     outDir: 'dist',
     assetsDir: 'assets',
   }
